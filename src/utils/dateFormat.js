8a2de67a6f88d1dcae7fbdf0c49ae496095232db
dknj11902 <dankim20@bergen.org>
2021-06-15 21:03:47 -0400

    Updates

--- src/utils/dateFormat.js
+++
@@ -1,9 +0,0 @@
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-export function formatMonthYear(dateObj) {
-    return months[dateObj.getMonth()] + " " + dateObj.getFullYear();
-}
-
-export function formatDayMonthYear(dateObj) {
-    return dateObj.getDate() + " " + formatMonthYear(dateObj);
-}
