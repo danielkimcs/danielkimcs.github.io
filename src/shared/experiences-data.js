49195c9c55cb7f17589a78f4b211756044f6b0cd
dknj11902 <dankim20@bergen.org>
2021-08-02 12:17:55 -0400

    Updated resume and experiences

--- src/shared/experiences-data.js
+++ src/shared/experiences-data.js
@@ -1,20 +1,20 @@
 const experiencesData = {
     data: [
         {
-            position: "Full Stack Associate",
+            position: "Software Engineer Intern",
             employer: "GoSite",
             startDate: new Date(2021, 5),
             endDate: null,
             location: "San Diego, CA",
-            description: ""
+            description: "I used the customer data platform Segment to increase product analytics coverage of an all-in-one cloud-based software suite by 15% to formulate behavioral cohorts and gain insights into drop-off rates and conversion funnels for the product management team to utilize. I also developed a recurring invoice feature, a standard add-on seen in competing products, with user-facing forms in React and payment and scheduled email systems on a serverless architecture in Node.js (with AWS Lambda) and MongoDB. I collaborated with software engineers under agile scrum methodologies with retrospective, grooming, and sprint planning sessions and daily stand-ups."
         },
         {
             position: "Full Stack Developer",
             employer: "Yale Computer Society",
-            startDate: new Date(2020, 9),
+            startDate: new Date(2020, 10),
             endDate: null,
             location: "New Haven, CT",
-            description: ""
+            description: "I led the frontend development of an application management system to support over 15 recruiters review over 600 applications for 200 job listings, making choices in modular design and state management logic. I also created data flow diagrams and implemented REST APIs to support robust information management systems via AWS Lambda and DynamoDB. I also integrated services Amazon CloudFront and CodePipeline to set up automated production deployment systems to increase scalability."
         },
         {
             position: "Research Intern",
@@ -22,7 +22,7 @@ const experiencesData = {
             startDate: new Date(2019, 8),
             endDate: new Date(2020, 4),
             location: "Piscataway, NJ",
-            description: "Under the mentorship of Dr. Daniel Balouek-Thomert, I studied stream processing between the edge and the cloud. I automated deployment of resources (operating systems, scripts, applications) via Python on Grid'5000 (a testbed for experiment-driven research), ran experiments with R-Pulsar (an Edge Framework developed by RDI2), and analyzed datasets via batch processing (Python - NumPy) and stream processing (Apache Flink)."
+            description: "Under the mentorship of Dr. Daniel Balouek-Thomert, I studied stream processing between the edge and the cloud. I wrote scripts in Python to automate the deployment of resources (operating systems and software) on various networked CPUs provided by Grid'5000, a testbed for experiment-driven research. I also ran experiments with R-Pulsar (an Edge Framework developed by RDI2) and analyzed datasets via batch processing (Python - NumPy) and stream processing (Apache Flink) to extract insights into New York City traffic."
         },
         {
             position: "Private Mathematics Tutor",
