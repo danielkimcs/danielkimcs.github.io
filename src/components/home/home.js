95683a0a7060d485eb79aabfeb9138d5ece8a9d9
Daniel Kim <dknj11902@gmail.com>
2022-11-22 16:56:44 -0500

    Updated home page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,6 +3,7 @@ import "./home.scss";
 // import projectsData from '../../shared/projects-data';
 // import archiveData from '../../shared/archive-data';
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 import { NavLink } from "react-router-dom";
@@ -13,8 +14,10 @@ import Image from "react-bootstrap/Image";
 // import { Parallax } from 'react-scroll-parallax';
 import { Controller, Scene } from "react-scrollmagic";
 import { formatMonthYear } from "../../utils/dateFormat";
-import profilePic from "../../shared/assets/images/me.jpg";
+import profilePic from "../../shared/assets/images/me.jpeg";
 // import Button from 'react-bootstrap/Button';
+import YaleLogo from "../../shared/assets/images/yale.jpeg";
+import BCALogo from "../../shared/assets/images/bca.png";
 
 import ReactGA from "react-ga";
 ReactGA.initialize("UA-88938949-5");
@@ -42,9 +45,9 @@ export const Home = () => {
                   I'm an undergraduate junior at{" "}
                   <a href="https://www.yale.edu/">Yale University</a> studying
                   in computer science and mathematics, with broad experience in
-                  application development, especially in full stack web.
-                  I've scoped and built out many external, user-facing web
-                  features throughout previous software engineer internships at{" "}
+                  application development, especially in full stack web. I've
+                  scoped and built out many external, user-facing web features
+                  throughout previous software engineer internships at{" "}
                   <a href="https://www.metacareers.com/">Meta (Facebook)</a> on
                   their recruiting products and at{" "}
                   <a href="https://www.gosite.com/">GoSite</a> on their software
@@ -79,80 +82,97 @@ export const Home = () => {
         </div>
         <Container>
           <Scene
-            triggerElement=".accordion"
+            triggerElement=".timeline"
             triggerHook={1}
             // reverse={false}
             classToggle="visible"
           >
             <Row className="timeline fadeIn">
-              <h2>Education</h2>
-              <Accordion defaultActiveKey="0">
-                <Card>
-                  <Card.Header>
-                    <Accordion.Toggle eventKey="0">
-                      <p className="h4">
-                        Bachelor of Science in Computer Science, Mathematics
-                      </p>
-                      <p className="h5">
-                        Yale University, September 2020 – May 2024
-                      </p>
-                    </Accordion.Toggle>
-                  </Card.Header>
-                  <Accordion.Collapse eventKey="0">
-                    <Card.Body>
-                      <p>
-                        <span>Coursework:</span> Intensive Algorithms, Quantum
-                        Computing, Big Data Systems: Trends &amp; Challenges,
-                        Systems Programming and Computer Organization,
-                        Artificial Intelligence, Data Structures and Programming
-                        Techniques, Theory of Statistics, Probability Theory,
-                        Discrete Mathematics, Real Analysis, Vector Calculus and
-                        Linear Algebra
-                      </p>
-                    </Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-                <Card>
-                  <Card.Header>
-                    <Accordion.Toggle eventKey="1">
-                      <p className="h4">High School Diploma</p>
-                      <p className="h5">
-                        Bergen County Academies – Academy for Technology and
-                        Computer Science, September 2016 - June 2020
-                      </p>
-                    </Accordion.Toggle>
-                  </Card.Header>
-                  <Accordion.Collapse eventKey="1">
-                    <Card.Body>
-                      <p>
-                        <span>Activities:</span> Problem Writer, Lecturer,
-                        Webmaster of Math Team; Committee Member and Webmaster
-                        for the Joe Holbrook Memorial Math Competition (400+
-                        annual participants); American Computer Science League
-                        (ACSL) Participant; Competitive Programming (USACO);
-                        Webmaster for BCA Flash; Varsity Bowling Team
-                      </p>
+              <h2 className="text-center mb-4">Education</h2>
+              <Container>
+                <Row className="education-entry">
+                  <Col
+                    xs={2}
+                    className="d-flex justify-content-center align-items-center company-logo-container"
+                  >
+                    <div className="company-logo">
+                      <Image src={YaleLogo} />
+                    </div>
+                  </Col>
+                  <Col>
+                    <Card className="education-card">
+                      <Card.Header>
+                        <p className="h4">
+                          Bachelor of Science in Computer Science, Mathematics
+                        </p>
+                        <p className="h5">
+                          Yale University, September 2020 – May 2024
+                        </p>
+                      </Card.Header>
+                      <Card.Body>
+                        <p>
+                          <span>Coursework:</span> Intensive Algorithms, Machine
+                          Learning, Quantum Computing, Big Data Systems, Systems
+                          Programming and Computer Organization, Artificial
+                          Intelligence, Data Structures and Programming
+                          Techniques, Theory of Statistics, Probability Theory,
+                          Discrete Mathematics, Real & Complex Analysis, Vector
+                          Calculus and Linear Algebra
+                        </p>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                </Row>
+                <Row className="education-entry">
+                  <Col
+                    xs={2}
+                    className="d-flex justify-content-center align-items-center company-logo-container"
+                  >
+                    <div className="company-logo">
+                      <Image src={BCALogo} />
+                    </div>
+                  </Col>
+                  <Col>
+                    <Card className="education-card">
+                      <Card.Header>
+                        <p className="h4">High School Diploma</p>
+                        <p className="h5">
+                          Bergen County Academies – Academy for Technology and
+                          Computer Science, September 2016 - June 2020
+                        </p>
+                      </Card.Header>
+                      {/* <Card.Body>
+                        <p>
+                          <span>Activities:</span> Problem Writer, Lecturer,
+                          Webmaster of Math Team; Committee Member and Webmaster
+                          for the Joe Holbrook Memorial Math Competition (400+
+                          annual participants); American Computer Science League
+                          (ACSL) Participant; Competitive Programming (USACO);
+                          Webmaster for BCA Flash; Varsity Bowling Team
+                        </p>
 
-                      <p>
-                        <span>Coursework:</span> Advanced Math Topics, Data
-                        Structures, Advanced Topics in Computer Science, AP
-                        Analytical Calculus (BC+), AP Computer Science A,
-                        Advanced Precalculus with Discrete Topics, Mathematical
-                        Structures and Proofs, AP Chemistry, AP Physics C
-                      </p>
-                    </Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-              </Accordion>
+                        <p>
+                          <span>Coursework:</span> Advanced Math Topics, Data
+                          Structures, Advanced Topics in Computer Science, AP
+                          Analytical Calculus (BC+), AP Computer Science A,
+                          Advanced Precalculus with Discrete Topics,
+                          Mathematical Structures and Proofs, AP Chemistry, AP
+                          Physics C
+                        </p>
+                      </Card.Body> */}
+                    </Card>
+                  </Col>
+                </Row>
+              </Container>
             </Row>
           </Scene>
-          {/* <Row className="timeline experience">
+          <Row className="timeline experience">
             <Scene
               triggerHook={0.9}
               // reverse={false}
               classToggle="visible"
             >
-              <h2 className="fadeIn">Experience</h2>
+              <h2 className="fadeIn text-center mb-4">Experience</h2>
             </Scene>
             <CardDeck>
               {experiences.map((exp, index) => (
@@ -164,40 +184,53 @@ export const Home = () => {
                     classToggle="visible"
                   >
                     <div className="fadeIn">
-                      <div className="exp-image">
-                        <Image src="https://media-exp1.licdn.com/dms/image/C4E0BAQFdNatYGiBelg/company-logo_200_200/0/1636138753911?e=1677110400&v=beta&t=pW_Ij3_e-dGk3d1nDPlYvIb50HGgyoo3t0MfCQOKGsA" />
-                      </div>
-                      <Card>
-                        <Card.Header>
-                          <p className="h4">
-                            {exp.position +
-                              (exp.employer.length > 0
-                                ? ", " + exp.employer
-                                : "")}
-                          </p>
-                          <p className="h5 exp-date">
-                            {formatMonthYear(exp.startDate) +
-                              " — " +
-                              (exp.endDate
-                                ? formatMonthYear(exp.endDate)
-                                : "present")}
-                          </p>
-                          <p className="h5 exp-location">{exp.location}</p>
-                        </Card.Header>
-                        <Card.Body>
-                          <p>
-                            {exp.description && exp.description.length > 0
-                              ? exp.description
-                              : "Description will be added"}
-                          </p>
-                        </Card.Body>
-                      </Card>
+                      <Row className="experience-entry">
+                        {exp.image ? (
+                          <Col
+                            xs={2}
+                            className="d-flex justify-content-center align-items-center company-logo-container"
+                          >
+                            <div className="company-logo">
+                              <Image
+                                src={process.env.PUBLIC_URL + "/" + exp.image}
+                              />
+                            </div>
+                          </Col>
+                        ) : null}
+                        <Col>
+                          <Card>
+                            <Card.Header>
+                              <p className="h4">
+                                {exp.position +
+                                  (exp.employer.length > 0
+                                    ? ", " + exp.employer
+                                    : "")}
+                              </p>
+                              <p className="h5 exp-date">
+                                {formatMonthYear(exp.startDate) +
+                                  " — " +
+                                  (exp.endDate
+                                    ? formatMonthYear(exp.endDate)
+                                    : "present")}
+                              </p>
+                              <p className="h5 exp-location">{exp.location}</p>
+                            </Card.Header>
+                            <Card.Body>
+                              <p>
+                                {exp.description && exp.description.length > 0
+                                  ? exp.description
+                                  : "Description will be added"}
+                              </p>
+                            </Card.Body>
+                          </Card>
+                        </Col>
+                      </Row>
                     </div>
                   </Scene>
                 </div>
               ))}
             </CardDeck>
-          </Row> */}
+          </Row>
         </Container>
       </Controller>
     </div>
--- src/shared/experiences-data.js
+++ src/shared/experiences-data.js
@@ -1,54 +1,72 @@
 const experiencesData = {
-    data: [
-        {
-            position: "Software Engineer Intern",
-            employer: "Meta (Facebook)",
-            startDate: new Date(2022, 4),
-            endDate: null,
-            location: "Menlo Park, CA",
-            description: "Incoming, Summer 2022"
-        },
-        {
-            position: "Software Engineer Intern",
-            employer: "GoSite",
-            startDate: new Date(2021, 5),
-            endDate: new Date(2021, 7),
-            location: "San Diego, CA",
-            description: "I used the customer data platform Segment to increase product analytics coverage of an all-in-one cloud-based software suite by 15% to formulate behavioral cohorts and gain insights into drop-off rates and conversion funnels for the product management team to utilize. I also developed a recurring invoice feature, a standard add-on seen in competing products, with user-facing forms in React and payment and scheduled email systems on a serverless architecture in Node.js (with AWS Lambda) and MongoDB. I collaborated with software engineers under agile scrum methodologies with retrospective, grooming, and sprint planning sessions and daily stand-ups."
-        },
-        {
-            position: "Frontend Project Lead",
-            employer: "Yale Entrepreneurial Society",
-            startDate: new Date(2020, 10),
-            endDate: new Date(2022, 4),
-            location: "New Haven, CT",
-            description: "I led the frontend development of an application management system to support over 15 recruiters review over 600 applications for 200 job listings, making choices in modular design and state management logic. I also created data flow diagrams and implemented REST APIs to support robust information management systems via AWS Lambda and DynamoDB. I also integrated services Amazon CloudFront and CodePipeline to set up automated production deployment systems to increase scalability."
-        },
-        {
-            position: "Research Intern",
-            employer: "Rutgers Discovery Informatics Institute",
-            startDate: new Date(2019, 8),
-            endDate: new Date(2020, 4),
-            location: "Piscataway, NJ",
-            description: "Under the mentorship of Dr. Daniel Balouek-Thomert, I studied stream processing between the edge and the cloud. I wrote scripts in Python to automate the deployment of resources (operating systems and software) on various networked CPUs provided by Grid'5000, a testbed for experiment-driven research. I also ran experiments with R-Pulsar (an Edge Framework developed by RDI2) and analyzed datasets via batch processing (Python - NumPy) and stream processing (Apache Flink) to extract insights into New York City traffic."
-        },
-        {
-            position: "Private Math Tutor",
-            employer: "",
-            startDate: new Date(2019, 3),
-            endDate: null,
-            location: "",
-            description: "I individually tutor high school students in precalculus, AP Calculus BC, and American Mathematics Competitions (AMC) concepts weekly. I review materials from school classes as well as help them prepare for future classes. I often create my own challenging homework problems to reinforce concepts and focus on helping students develop problem-solving intuition."
-        },
-        {
-            position: "Python Coding Instructor",
-            employer: "Gauss Academy of Mathematical Education",
-            startDate: new Date(2020, 2),
-            endDate: new Date(2020, 3),
-            location: "Springfield, NJ",
-            description: "I remotely taught GUI programming in Python (Turtle, Tkinter, Canvas) as part of a virtual workshop, covering classic examples like tic-tac-toe, Snake, and Guess the Number. I planned course materials, assigned homework, and held classes via Zoom 3 days a week."
-        }
-    ]
+  data: [
+    {
+        position: "Software Engineer Intern",
+        employer: "Optiver",
+        startDate: new Date(2023, 5),
+        endDate: null,
+        location: "Chicago, IL",
+        description: "Incoming, Summer 2023",
+        image: "optiverlogo.jpeg",
+      },
+    {
+      position: "Software Engineer Intern",
+      employer: "Meta (Facebook)",
+      startDate: new Date(2022, 4),
+      endDate: new Date(2022, 7),
+      location: "Menlo Park, CA",
+      description: "I scoped and implemented two web features on Meta's Career Site and Career Profile — pre-fill form switch and one-click quick apply — to streamline the job application process on a React/Redux frontend, Hack/Ent Schema backend, and a GraphQL layer in between. I took initiative to propose and lead beta testing sessions and contributed over 17,000 non-generated, significant lines of code in 10 weeks.",
+      image: "metalogo.jpeg",
+    },
+    {
+      position: "Software Engineer Intern",
+      employer: "GoSite",
+      startDate: new Date(2021, 5),
+      endDate: new Date(2021, 7),
+      location: "San Diego, CA",
+      description:
+        "I used the customer data platform Segment to increase product analytics coverage of an all-in-one cloud-based software suite by 15% to provide additional metrics for the product management team. I also developed a recurring invoice feature with user-facing forms in React and payment and scheduled email systems on a serverless architecture in Node.js (with AWS Lambda) and MongoDB. I collaborated with engineers under agile scrum methodologies with retrospective, grooming, and sprint planning sessions and daily stand-ups.",
+      image: "gositelogo.jpeg",
+    },
+    {
+      position: "Full Stack Developer",
+      employer: "Yale Entrepreneurial Society",
+      startDate: new Date(2020, 10),
+      endDate: new Date(2022, 4),
+      location: "New Haven, CT",
+      description:
+        "I led the frontend development of an application management system to support over 15 recruiters review over 600 applications for 200 job listings, making choices in modular design and state management logic. I also created data flow diagrams and implemented REST APIs to support robust information management systems via AWS Lambda and DynamoDB. I also integrated services Amazon CloudFront and CodePipeline to set up automated production deployment systems to increase scalability.",
+      image: "yeslogo.jpeg",
+    },
+    {
+      position: "Research Intern",
+      employer: "Rutgers Discovery Informatics Institute",
+      startDate: new Date(2019, 8),
+      endDate: new Date(2020, 4),
+      location: "Piscataway, NJ",
+      description:
+        "Under the mentorship of Dr. Daniel Balouek-Thomert, I studied stream processing between the edge and the cloud. I wrote scripts in Python to automate the deployment of resources (operating systems and software) on various networked CPUs provided by Grid'5000, a testbed for experiment-driven research. I also ran experiments with R-Pulsar (an Edge Framework developed by RDI2) and analyzed datasets via batch processing (Python - NumPy) and stream processing (Apache Flink) to extract insights into New York City traffic.",
+      image: "rutgerslogo.jpeg",
+    },
+    {
+      position: "Private Math Tutor",
+      employer: "",
+      startDate: new Date(2019, 3),
+      endDate: null,
+      location: "",
+      description:
+        "I individually tutor high school students in precalculus, AP Calculus BC, and American Mathematics Competitions (AMC) concepts weekly. I review materials from school classes as well as help them prepare for future classes. I often create my own challenging homework problems to reinforce concepts and focus on helping students develop problem-solving intuition.",
+    },
+    {
+      position: "Python Coding Instructor",
+      employer: "Gauss Academy of Mathematical Education",
+      startDate: new Date(2020, 2),
+      endDate: new Date(2020, 3),
+      location: "Springfield, NJ",
+      description:
+        "I remotely taught GUI programming in Python (Turtle, Tkinter, Canvas) as part of a virtual workshop, covering classic examples like tic-tac-toe, Snake, and Guess the Number. I planned course materials, assigned homework, and held classes via Zoom 3 days a week.",
+    },
+  ],
 };
 
 export default experiencesData;
