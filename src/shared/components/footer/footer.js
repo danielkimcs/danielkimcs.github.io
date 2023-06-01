09b8b4bf8648ff95190c0616227902c6cb0a9204
Daniel Kim <dknj11902@gmail.com>
2023-06-01 16:06:23 -0400

    year change

--- src/shared/components/footer/footer.js
+++ src/shared/components/footer/footer.js
@@ -5,7 +5,7 @@ import { SocialIcon } from "react-social-icons";
 export const Footer = () => {
     return (
         <div className="footer-container">
-            <div className="footer-text">&copy; 2022 &bull; <span>Daniel Kim</span></div>
+            <div className="footer-text">&copy; 2023 &bull; <span>Daniel Kim</span></div>
             <div className="social-container">
                 {/* Add prop bgColor="dark" to change background color of a SocialIcon component */}
                 <SocialIcon target="_blank" rel="noreferrer" className="social-icon" style={{ height: 30, width: 30 }} bgColor="black" url="https://www.linkedin.com/in/danielkim119/" />
